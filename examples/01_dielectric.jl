646d0a0a14019387c5635b706a77887ceffd6f15
Oliver Kliebisch <oliver@kliebisch.net>
2022-10-31 10:17:30 +0100

    More docs

--- examples/01_dielectric.jl
+++ examples/01_dielectric.jl
@@ -4,11 +4,17 @@ x = LinRange(0.1, 100, 300)
 qext, qsca, qback, g = mie(1.5, x)
 
 fig = Figure()
-ax = fig[1,1] = Axis(fig, xlabel="Size parameter (-)", ylabel="Qext", title="Comparison of extinction for absorbing and non-absorbing spheres")
-lines!(x, qext, color=:red, label="1.5")
+ax =
+    fig[1, 1] = Axis(
+        fig,
+        xlabel = "Size parameter (-)",
+        ylabel = "Qext",
+        title = "Comparison of extinction for absorbing and non-absorbing spheres",
+    )
+lines!(x, qext, color = :red, label = "1.5")
 
-qext, qsca, qback, g = mie(1.5 -0.1*im, x)
-lines!(x, qext, color=:blue, label="1.5-0.1j")
+qext, qsca, qback, g = mie(1.5 - 0.1 * im, x)
+lines!(x, qext, color = :blue, label = "1.5-0.1j")
 
 axislegend()
 fig
