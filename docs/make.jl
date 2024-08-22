c8040dd2ad70bfa371887735ae71aeeae636529f
Oliver Kliebisch <oliver@kliebisch.net>
2024-08-22 20:48:07 +0200

    Try docs fix

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,6 @@ using MieScattering
 makedocs(
     modules = [MieScattering],
     authors = "Oliver Kliebisch <oliver@kliebisch.net> and contributors",
-    repo = "https://github.com/tachawkes/miescattering.jl/blob/{commit}{path}#L{line}",
     sitename = "MieScattering.jl",
     format = Documenter.HTML(;
         prettyurls = get(ENV, "CI", "false") == "true",
